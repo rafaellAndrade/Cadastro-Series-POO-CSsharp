fb5e2bf2aefce266f20234c6d48374e11271b6cf
rafaellAndrade <rafael_system_metal@yahoo.com.br>
2022-01-12 18:30:45 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+
+namespace CadastroSeries
+{
+    class Program
+    {
+        static SerieRepositorio repositorio = new SerieRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while(opcaoUsuario.ToUpper() != "X")
+            {
+                switch(opcaoUsuario)
+                {
+                    case "1":
+                    ListarSeries();
+                    break;
+
+                    case "2":
+                    InserirSerie();
+                    break;
+
+                    case "3":
+                    AtualizarSerie();
+                    break;
+
+                    case "4":
+                    ExcluirSerie();
+                    break;
+                    
+                    case "5":
+                    VisualizarSerie();
+                    break;
+
+                    case "C":
+                    System.Console.Clear();
+                    break;
+
+                    default:
+                    System.Console.WriteLine("Opção Inválida");
+                    break;
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            System.Console.WriteLine("Obrigado por utilizar os nossos serviços. S2");
+            Console.ReadLine();        
+        }
+
+        private static void ListarSeries()
+        {
+            System.Console.WriteLine("Listar séries");
+            System.Console.WriteLine();
+
+            var lista  = repositorio.Lista();
+
+            if(lista.Count == 0)
+            {
+                System.Console.WriteLine("Nenhuma série cadastrada.");
+                return;
+            }
+
+            foreach(var serie in lista)
+            {
+                var excluido = serie.retornaExcluido();
+                
+                System.Console.WriteLine("#ID {0}: - {1}  {2}", serie.retornaId(), serie.retornaTitulo(), (excluido ? "Excluído" : ""));
+            }
+        }
+
+        private static void InserirSerie()
+        {
+            System.Console.WriteLine("Inserir nova série");
+
+            foreach(int i in Enum.GetValues(typeof(Genero)))
+            {
+                System.Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            
+            System.Console.WriteLine("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o titulo da série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            System.Console.WriteLine("Digite o ano de início da série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite a descrição da série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie novaSerie = new Serie(id: repositorio.ProximoId(),
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+
+            repositorio.Insere(novaSerie);
+        }
+
+        private static void AtualizarSerie()
+        {
+            System.Console.WriteLine("Digite o id da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            foreach(int i in Enum.GetValues(typeof(Genero)))
+            {
+                System.Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+
+            System.Console.WriteLine("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o titulo da série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            System.Console.WriteLine("Digite o ano de início da série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite a descrição da série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie atualizaSerie = new Serie(id: indiceSerie,
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+
+            repositorio.Atualiza(indiceSerie, atualizaSerie);
+        }
+
+        private static void ExcluirSerie()
+        {
+            System.Console.WriteLine("Digite o id da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            repositorio.Exclui(indiceSerie);
+        }
+
+        private static void VisualizarSerie()
+        {
+            System.Console.WriteLine("Digite o id da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            var serie = repositorio.RetornaPorId(indiceSerie);
+
+            System.Console.WriteLine();
+            System.Console.WriteLine(serie);
+        }
+        private static string ObterOpcaoUsuario()
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine("Rafa Séries!!");
+            System.Console.WriteLine();
+            System.Console.WriteLine("Informe a opção desejada:");
+            System.Console.WriteLine();
+
+            System.Console.WriteLine("1- Listar séries");
+            System.Console.WriteLine("2- Inserir nova série");
+            System.Console.WriteLine("3- Atualizar série");
+            System.Console.WriteLine("4- Excluir série");
+            System.Console.WriteLine("5- Visualizar série");
+            System.Console.WriteLine("C- Limpar Tela");
+            System.Console.WriteLine("X- Sair");
+            System.Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            System.Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
